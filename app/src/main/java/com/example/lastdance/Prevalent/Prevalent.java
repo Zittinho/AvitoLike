f8bbc157bfd206ec1fd135ba53c7b69ea374beb5
Zittinho <titanzittoyt2@mail.ru>
2024-04-14 23:22:18 +1000

    dsadsa

---
+++ app/src/main/java/com/example/lastdance/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.lastdance.Prevalent;
+
+import com.example.lastdance.Models.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser = new Users();
+
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
---
+++ app/src/main/java/com/example/lastdance/interfac/ItemClickListner.java
@@ -0,0 +1,7 @@
+package com.example.lastdance.interfac;
+
+import android.view.View;
+
+public interface ItemClickListner {
+    void onClick(View view, int position, boolean isLongClick);
+}
